19b3ea02884ef800a8ff2f557e972ec68426831b
GiraMorales <gira.morales@gamil.com>
2025-06-27 17:45:20 +0200

    first commit

---
+++ ReactBasics/src/Components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Website Title</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
